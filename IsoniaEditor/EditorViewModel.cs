b0e683c357e78993b0e809726e64d007f1b67d72
Simon <Zhyosgames@gmail.com>
2023-12-05 20:52:49 +0100

    more menu fixes

--- IsoniaEditor/EditorViewModel.cs
+++ IsoniaEditor/EditorViewModel.cs
@@ -1,4 +1,8 @@
-﻿using IsoniaCore.DataTypes;
+﻿using IsoniaCore.Resources.Icons;
+using IsoniaCore.ViewModels;
+using IsoniaCore.DataTypes;
+using Avalonia.Input;
+using System.Collections.ObjectModel;
 using System.Reflection;
 using System;
 
@@ -12,11 +16,35 @@ public class EditorViewModel : Observable
     public Version? Version => version;
     public string? Title => title;
 
-
-    public string[] test => new string[] { "asdf", "asdfashgas" };
-
+    public ObservableCollection<MenuItemViewModel> Test { get; }
 
     public EditorViewModel()
     {
+        Test = new ObservableCollection<MenuItemViewModel>()
+        {
+            new(string.Empty, IconStore.ProjectIcon, null, null,
+                new MenuItemViewModel("Build And Run")
+            ),
+            new("File", null, null, null,
+                new MenuItemViewModel("New Scene"),
+                new MenuItemViewModel("Open Scene"),
+
+                new MenuItemViewModel("Save Scene", IconStore.SaveIcon, null, new KeyGesture(Key.S, KeyModifiers.Control)),
+                new MenuItemViewModel("Save Scene As...", IconStore.SaveAsIcon, null, new KeyGesture(Key.S, KeyModifiers.Control | KeyModifiers.Shift))
+            ),
+            new("Edit", null, null, null,
+                new MenuItemViewModel("Undo"),
+                new MenuItemViewModel("Redo"),
+                new MenuItemViewModel("Undo History"),
+
+                new MenuItemViewModel("Cut"),
+                new MenuItemViewModel("Copy"),
+                new MenuItemViewModel("Paste"),
+                new MenuItemViewModel("Duplicate"),
+                new MenuItemViewModel("Delete")
+            ),
+            new("Window"),
+            new("Tools")
+        };
     }
 }
